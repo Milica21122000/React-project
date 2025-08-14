815657eb9b0c362bbe992e3c6008a6e6377de4c0
unknown <mmilica.vvujinovic@gmail.com>
2025-08-14 15:56:41 +0200

    Add State to job listings component

--- react-app/src/components/JobListing.jsx
+++ react-app/src/components/JobListing.jsx
@@ -1,5 +1,16 @@
+import { useState } from "react";
+import {FaMapMarker} from "react-icons/fa"
 
 const JobListing = ({job}) => {
+
+const [ShowFullDescription, setShowFullDescription] = useState(false);
+
+let description = job.description;
+
+if (!ShowFullDescription){
+    description = description.substring(0, 90) + '...';
+}
+
  return (
      <div className="bg-white rounded-xl shadow-md relative">
             <div className="p-4">
@@ -9,8 +20,14 @@ const JobListing = ({job}) => {
               </div>
 
               <div className="mb-5">
-               {job.description}
+               {description}
               </div>
+
+              <button
+              className="text-indigo-500 mb-5 hover:text-indigo-600"  
+              onClick={() => setShowFullDescription((prevState => !prevState))}>
+              {ShowFullDescription ? 'Show Less' : 'Show More'}
+              </button>
 
               <h3 className="text-indigo-500 mb-2">{job.salary}</h3>
 
@@ -18,8 +35,9 @@ const JobListing = ({job}) => {
 
               <div className="flex flex-col lg:flex-row justify-between mb-4">
                 <div className="text-orange-700 mb-3">
+                    <FaMapMarker className="inline text-lg mb-1 mr-1"/>
                   <i className="fa-solid fa-location-dot text-lg"></i>
-                  Boston, MA
+                  {job.location}
                 </div>
                 <a
                   href={`/job/${job.id}`}
