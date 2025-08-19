430a95c9e9d7deeab55de7d7740c7c0a4641cea0
unknown <mmilica.vvujinovic@gmail.com>
2025-08-19 09:53:15 +0200

    Addes pages

--- react-app/src/components/JobListing.jsx
+++ react-app/src/components/JobListing.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import {FaMapMarker} from "react-icons/fa"
+import { Link } from "react-router-dom";
 
 const JobListing = ({job}) => {
 
@@ -39,12 +40,12 @@ if (!ShowFullDescription){
                   <i className="fa-solid fa-location-dot text-lg"></i>
                   {job.location}
                 </div>
-                <a
-                  href={`/job/${job.id}`}
+                <Link
+                  to={`/job/${job.id}`}
                   className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                 >
                  Read More
-                </a>
+                </Link>
               </div>
             </div>
           </div>
